82a26f7ef70331076d92dfa178d6121a6372cd10
Matteo Pagani <matteo.pagani@microsoft.com>
2021-01-30 14:00:19 +0100

    First commit

---
+++ windows/RN064/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::RN064::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::RN064::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
